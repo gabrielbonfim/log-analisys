3e2be8dcbff745211fbae1882049f52e90ca6931
Gabriel Almeida <gabrielbonfim@gmail.com>
2018-03-01 16:58:37 +0000

    Line length correction

--- log-analysis.py
+++ log-analysis.py
@@ -15,12 +15,29 @@ import psycopg2
 # Constants with database name, querys and titles
 DBNAME = "news"
 APP_TITLE = "\n\nLog Analysis"
-TOP_ARTICLES_QUERY = "select a.title, t.count from articles a, articles_top_desc t where a.slug like t.slug limit 3"
+TOP_ARTICLES_QUERY = """
+  select a.title, t.count from articles a, articles_top_desc t where a.slug
+  like t.slug limit 3
+"""
 TOP_ARTICLE_TITLE = "\n\nThe most popular three articles of all time are:\n"
-TOP_AUTHORS_QUERY = "select au.name as author, sum(top.count) as count from authors au, articles ar, articles_top_desc top where au.id = ar.author and ar.slug = top.slug group by au.name order by count desc"
+TOP_AUTHORS_QUERY = """
+  select au.name as author, sum(top.count) as count from authors au,
+  articles ar, articles_top_desc top where au.id = ar.author and
+  ar.slug = top.slug group by au.name order by count desc
+"""
 TOP_AUTHORS_TITLE = "\n\nThe most popular article authors of all time are:\n"
-ACCESS_ERRORS_QUERY = "select to_char(d.day, 'Month DD, YYYY'), d.percentage from (select l200.day, l200.count, l404.count, round(((l404.count * 100)::numeric / (l200.count + l404.count)::numeric),1) as percentage from (select date_trunc('day', time) as day, count(*) as count from log where status like '200 OK' group by day) as l200, (select date_trunc('day', time) as day, count(*) as count from log where status like '404 NOT FOUND' group by day) as l404 where l200.day = l404.day order by percentage desc) as d where percentage > 1"
-ACCESS_ERRORS_TITLE = "\n\nDays with more than 1% of requests that lead to errors:\n"
+ACCESS_ERRORS_QUERY = """
+  select to_char(d.day, 'Month DD, YYYY'), d.percentage from (select l200.day,
+  l200.count, l404.count, round(((l404.count * 100)::numeric / (l200.count +
+  l404.count)::numeric),1) as percentage from (select date_trunc('day', time)
+  as day, count(*) as count from log where status like '200 OK' group by day)
+  as l200, (select date_trunc('day', time) as day, count(*) as count from log
+  where status like '404 NOT FOUND' group by day) as l404 where l200.day =
+  l404.day order by percentage desc) as d where percentage > 1
+"""
+ACCESS_ERRORS_TITLE = """
+  \n\nDays with more than 1% of requests that lead to errors:\n
+"""
 FOOTER = "\n\nGabriel Almeida - Udacity Student - v1.0\n"
 
 # Print the app title
